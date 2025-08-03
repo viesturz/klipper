0642ccf55c3f494c60864387c2bc6dbdb87b8637
Viesturs Zarins <viesturz@gmail.com>
2025-08-03 12:42:00 +0200

    Add is_shutdown handler.

--- klippy2/src/nativeMain/kotlin/mcu/McuBasics.kt
+++ klippy2/src/nativeMain/kotlin/mcu/McuBasics.kt
@@ -10,15 +10,20 @@ class McuBasics(val mcu: McuImpl, val configure: McuConfigure): McuComponent {
     init {
         configure.responseHandler<ResponseStats>( 0u, this::onStats)
         configure.responseHandler<ResponseShutdown>( 0u, this::onShutdown)
+        configure.responseHandler<ResponseIsShutdown>( 0u, this::onIsShutdown)
     }
 
     fun onStats(stats: ResponseStats) {
         logger.trace { "Stats: $stats" }
     }
     fun onShutdown(resp: ResponseShutdown) {
-        val reason = configure.firmware.enumerationIdToValue("static_string_id")[resp.staticStringId.toInt()]  ?: "unknown Firmware error ${resp.staticStringId}"
+        val reason = configure.firmware.getStaticString(resp.staticStringId, "unknown Firmware error ${resp.staticStringId}")
         logger.error {  "Shutdown Firmware error: $reason" }
         mcu.shutdown("MCU ${mcu.config.name}: $reason")
+    }
+    fun onIsShutdown(resp: ResponseIsShutdown) {
+        val reason = configure.firmware.getStaticString(resp.staticStringId, "unknown Firmware error ${resp.staticStringId}")
+        logger.error {  "Shutdown Firmware error: $reason" }
     }
 }
 
@@ -26,3 +31,5 @@ class McuBasics(val mcu: McuImpl, val configure: McuConfigure): McuComponent {
 data class ResponseStats(val count:UInt, val sum: UInt, val sumSq: UInt): McuResponse
 @RegisterMcuMessage(signature = "shutdown clock=%u static_string_id=%hu")
 data class ResponseShutdown(val clock32: McuClock32, val staticStringId: UShort): McuResponse
+@RegisterMcuMessage(signature = "is_shutdown static_string_id=%hu")
+data class ResponseIsShutdown(val staticStringId: UShort): McuResponse
